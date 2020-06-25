9282bd02b810e2e097465c92d05b8916c684807b
Ahmed-GiGA <67020269+Ahmed-GiGA@users.noreply.github.com>
2020-06-25 18:14:18 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: 'NzE5NjA4MTA0NDA4MzE4MDMy.XvS6oQ.WPk5Msv-cvCWL2IZ1Zrsh6rCjKA',// التوكن حق البوت   
+    TOKEN: 'NzE5NjA4MTA0NDA4MzE4MDMy.XvS_Lg.On39TsFxS2SGJrk9_JynCtnZyf4',// التوكن حق البوت   
     YT_API_KEY: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4',//حط اي بي اي مناسب
     prefix: '%',//برفيكس البوت
     devs: ['588434790655590427']//ايديك
